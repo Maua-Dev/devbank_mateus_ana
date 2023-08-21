33044e058976d4ea7139099f3f1ca3e4039e6f40
Ana Beatriz Schlink <abnschlink@gmail.com>
2023-08-21 11:37:48 -0300

    add validation withdraw

--- tests/app/entities/test_transactions.py
+++ tests/app/entities/test_transactions.py
@@ -21,27 +21,46 @@ class Test_Transactions:
 
         with pytest.raises(ParamNotValidated):
             Transactions(TransactionsTypeEnum.DEPOSIT.value, value, 100.00, 1004901390.00)
+        
+        with pytest.raises(ParamNotValidated):
+            Transactions(TransactionsTypeEnum.WITHDRAW.value,
+            value, 100.00, 1004901390.00)
 
     def test_validate_negative_value(self):
         value = -100.00
 
         with pytest.raises(ParamNotValidated):
             Transactions(TransactionsTypeEnum.DEPOSIT.value, value, 100.00, 1004901390.00)
+        
+        with pytest.raises(ParamNotValidated):
+            Transactions(TransactionsTypeEnum.WITHDRAW.value,
+            value, 100.00, 1004901390.00)
     
     def test_validate_current_balance(self):
         current_balance = "100.00"
 
         with pytest.raises(ParamNotValidated):
             Transactions(TransactionsTypeEnum.DEPOSIT.value, 100.00, current_balance, 1004901390.00)
+
+        with pytest.raises(ParamNotValidated):
+            Transactions(TransactionsTypeEnum.WITHDRAW.value,
+            100.00, current_balance, 1004901390.00)
     
     def test_validate_negative_current_balance(self):
         current_balance = -100.00
 
         with pytest.raises(ParamNotValidated):
             Transactions(TransactionsTypeEnum.DEPOSIT.value, 100.00, current_balance, 1004901390.00)
+
+        with pytest.raises(ParamNotValidated):
+            Transactions(TransactionsTypeEnum.WITHDRAW.value,
+            100.00, current_balance, 1004901390.00)
     
     def test_validate_timestamp(self):
         timestamp = "1004901390.00"
 
         with pytest.raises(ParamNotValidated):
             Transactions(TransactionsTypeEnum.DEPOSIT.value, 100.00, 100.00, timestamp)
+
+        with pytest.raises(ParamNotValidated):
+            Transactions(TransactionsTypeEnum.WITHDRAW.value, 100.00, 100.00, timestamp)
